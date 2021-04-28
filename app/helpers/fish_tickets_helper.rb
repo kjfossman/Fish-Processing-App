0c0e148f7475913c884bdcfd0d96c2048ff0f990
Kyle <kjfossman@gmail.com>
2021-04-28 15:58:20 -0800

    Added to the home page to display total pounds for the year

--- app/helpers/fish_tickets_helper.rb
+++ app/helpers/fish_tickets_helper.rb
@@ -1,11 +1,34 @@
 module FishTicketsHelper
 
-    def boat_spec?(boat)
-        if boat 
-        end
+    def total_pounds
+        total = FishTicket.sum(:chum_pounds) + FishTicket.sum(:sockeye_pounds) + FishTicket.sum(:coho_pounds) + FishTicket.sum(:humpy_pounds) + FishTicket.sum(:king_pounds)
+        total.to_s(:delimited)
     end
 
+    def total_chum_pounds
+        total = FishTicket.sum(:chum_pounds)
+        total.to_s(:delimited)
+    end
 
+    def total_sockeye_pounds
+        total = FishTicket.sum(:sockeye_pounds)
+        total.to_s(:delimited)
+    end
+
+    def total_coho_pounds
+        total = FishTicket.sum(:coho_pounds)
+        total.to_s(:delimited)
+    end
+
+    def total_humpy_pounds
+        total = FishTicket.sum(:humpy_pounds)
+        total.to_s(:delimited)
+    end
+
+    def total_king_pounds
+        total = FishTicket.sum(:king_pounds)
+        total.to_s(:delimited)
+    end
 
 
 end
